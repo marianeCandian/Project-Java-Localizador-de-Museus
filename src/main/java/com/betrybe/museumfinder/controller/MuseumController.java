e58ecca06706643d6bfd26fddd94ea9bdf38885b
Mariane Candian <mariane.candian@gmail.com>
2023-08-09 15:27:19 -0300

    feat: Criando o metodo getMuseum para rota GET - Requisito 6

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -2,16 +2,17 @@ package com.betrybe.museumfinder.controller;
 
 import com.betrybe.museumfinder.dto.MuseumCreationDto;
 import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.service.MuseumServiceInterface;
 import com.betrybe.museumfinder.util.ModelDtoConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +38,18 @@ public class MuseumController {
     Museum dto = ModelDtoConverter.dtoToModel(museum);
     Museum create = museumServiceInterface.createMuseum(dto);
     return ModelDtoConverter.modelToDto(create);
+  }
+
+  /**
+   * Metodo para buscar pelo Museum mais próximo.
+   */
+  @GetMapping("/closest")
+  @ResponseStatus(HttpStatus.OK)
+  public MuseumDto getMuseum(@RequestParam(name = "lat") double latitude,
+      @RequestParam(name = "lng") double longitude,
+      @RequestParam(name = "max_dist_km") double maxDistance) {
+    Coordinate coordinate = new Coordinate(latitude, longitude);
+    Museum result = museumServiceInterface.getClosestMuseum(coordinate, maxDistance);
+    return ModelDtoConverter.modelToDto(result);
   }
 }
