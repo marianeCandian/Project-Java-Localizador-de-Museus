c901a39fc6fb156471d07d2c5365f9d2254bb68f
Mariane Candian <mariane.candian@gmail.com>
2024-05-07 16:16:37 -0300

    modified MuseumControllerTest e MusuemServiceTest

--- src/test/java/com/betrybe/museumfinder/solution/MuseumControllerTest.java
+++ src/test/java/com/betrybe/museumfinder/solution/MuseumControllerTest.java
@@ -1,2 +1,70 @@
-package com.betrybe.museumfinder.solution;public class MuseumControllerTest {
+package com.betrybe.museumfinder.solution;
+
+import com.betrybe.museumfinder.controller.MuseumController;
+import com.betrybe.museumfinder.exception.MuseumNotFoundException;
+import com.betrybe.museumfinder.model.Coordinate;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.service.MuseumServiceInterface;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+// ... imports
+
+public class MuseumControllerTest {
+
+  @Mock
+  private MuseumServiceInterface museumService;
+
+  @InjectMocks
+  private MuseumController museumController;
+
+  private MockMvc mockMvc;
+
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+    mockMvc = MockMvcBuilders.standaloneSetup(museumController).build();
+  }
+
+  @Test
+  public void gettingMuseumSuccessfully() throws Exception {
+    Long museumId = 1L;
+    Museum requiredMuseum = new Museum();
+    requiredMuseum.setId(museumId);
+    requiredMuseum.setName("Goku Museum");
+    requiredMuseum.setDescription("Dragon Ball museum");
+    requiredMuseum.setAddress("Tokyo, Japan");
+    requiredMuseum.setCollectionType("history");
+    requiredMuseum.setCoordinate(new Coordinate(23.1254, -46.4051));
+
+    when(museumService.getMuseum(museumId)).thenReturn(requiredMuseum);
+
+    mockMvc.perform(get("/museums/{id}", museumId))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.id").value(requiredMuseum.getId()))
+        .andExpect(jsonPath("$.name").value(requiredMuseum.getName()))
+        .andExpect(jsonPath("$.description").value(requiredMuseum.getDescription()))
+        .andExpect(jsonPath("$.address").value(requiredMuseum.getAddress()))
+        .andExpect(jsonPath("$.collectionType").value(requiredMuseum.getCollectionType()));
+  }
+
+  @Test
+  public void gettingMuseumNotFound() throws Exception {
+    when(museumService.getMuseum(anyLong())).thenThrow(MuseumNotFoundException.class);
+    String url = "/museums/999";
+    mockMvc.perform(get(url))
+        .andExpect((status().isNotFound()));
+  }
 }
--- src/test/java/com/betrybe/museumfinder/solution/MuseumServiceTest.java
+++ src/test/java/com/betrybe/museumfinder/solution/MuseumServiceTest.java
@@ -1,2 +1,59 @@
-package com.betrybe.museumfinder.solution;public class MuseumServiceTest {
+package com.betrybe.museumfinder.solution;
+
+import com.betrybe.museumfinder.database.MuseumFakeDatabase;
+import com.betrybe.museumfinder.exception.MuseumNotFoundException;
+import com.betrybe.museumfinder.model.Coordinate;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.service.MuseumService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+public class MuseumServiceTest {
+
+  @Mock
+  private MuseumFakeDatabase museumFakeFakeDatabase;
+
+  @InjectMocks
+  private MuseumService museumServ;
+
+  @BeforeEach
+  public void initSetting() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void gettingMuseumSuccessfully() {
+    Long museumId = 1L;
+    Coordinate coordinate = new Coordinate(23.1254, -46.4051);
+    Museum requiredMuseum = new Museum();
+    requiredMuseum.setId(museumId);
+    requiredMuseum.setName("Goku Museum");
+    requiredMuseum.setDescription("Dragon Ball museum");
+    requiredMuseum.setAddress("Tokyo, Japan");
+    requiredMuseum.setCollectionType("history");
+    requiredMuseum.setCoordinate(coordinate);
+
+    when(museumFakeFakeDatabase.getMuseum(museumId)).thenReturn(Optional.of(requiredMuseum));
+
+    Museum museum = museumServ.getMuseum(museumId);
+
+    assertEquals(requiredMuseum, museum);
+  }
+
+  @Test
+  public void gettingMuseumNotFound() {
+    Long museumId = 1L;
+    when(museumFakeFakeDatabase.getMuseum(museumId)).thenReturn(Optional.empty());
+
+    assertThrows(MuseumNotFoundException.class, () -> museumServ.getMuseum(museumId));
+  }
 }
